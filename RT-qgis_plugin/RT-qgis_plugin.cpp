af841ee7d96d80e4e9031a96181b2a1336db0667
geoin <geoin@geoin-VirtualBox.(none)>
2014-07-07 16:14:07 +0200

    linux script

--- RT-qgis_plugin/RT-qgis_plugin.cpp
+++ RT-qgis_plugin/RT-qgis_plugin.cpp
@@ -50,7 +50,7 @@
 #include <QDesktopServices>
 #include <QUrl>
 
-#define RT_PLUGIN_VERSION "1.2.3"
+#define RT_PLUGIN_VERSION "1.2.4"
 
 #ifdef WIN32
 #define QGISEXTERN extern "C" __declspec( dllexport )
