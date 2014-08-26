be401ce81b81fbd89a96cda107f99879f50dc97b
umberto <u.califano@geoin.it>
2014-08-26 10:24:31 +0200

    test

--- test/dem_interpolate.cpp
+++ test/dem_interpolate.cpp
@@ -3,6 +3,7 @@
 #include <dem_interpolate/dsm.h>
 
 #include <iostream>
+#include <fstream>
 
 const std::string path = "/home/umberto/projects/rt/ControlloVoliRT/test/data/";
 
@@ -15,8 +16,13 @@ TEST (DemTest, Triangulation) {
     ASSERT_EQ(39835, d->Npt());
     ASSERT_EQ(79140, d->Ntriangle());
 
+    std::string npOut = path + "tria.np";
+    std::fstream out(npOut.c_str(), std::fstream::out);
     for (unsigned int i = 0; i < d->Ntriangle(); i++) {
         const TRIANGLE t = d->Triangle(i);
+        out << "N: [" << t.n[0] << ", " << t.n[1] << ", " << t.n[2] << "] - ";
+        out << "P: [" << t.p[0] << ", " << t.p[1] << ", " << t.p[2] << "]";
+        out << std::endl;
     }
 
     f.Close();
